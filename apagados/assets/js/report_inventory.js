98e8284b7c68af3fe30e47296c0f70698be83c34
rscirilo <rscirilo@gmail.com>
2023-04-27 23:21:13 -0300

    Delete
    apaguei arquivos desnecessarios

--- apagados/assets/js/report_inventory.js
+++
@@ -1,8 +0,0 @@
-function openPopup(obj) {
-	var data = $(obj).serialize();
-
-	var url = BASE_URL+"/report/inventory_pdf?"+data;
-	window.open(url, "report", "width=700,height=500");
-
-	return false;
-}
--- apagados/assets/js/script_inventory_add.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-
-	$('input[name=price]').mask('000.000.000.000.000,00', {reverse:true, placeholder:"0,00"});
-
-});
